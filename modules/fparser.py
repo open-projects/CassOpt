8b563d37411c8654def22adcbdeaca70ba26ff64
Dmitry Malko <31127521+open-projects@users.noreply.github.com>
2020-02-21 18:10:06 +0300

    fasta output file with casstete seq

---
+++ modules/fparser.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import re
+
+class FastaParser:
+    def __init__(self, file_name):
+        self._fasta = {}
+        with open(file_name, "r") as infile:
+            header = ""
+            for line in infile:
+                line = line.strip()
+                h_pattern = re.match(">(\S+)\D+(\d+)\.\.(\d+)", line)
+                if h_pattern:
+                    header = h_pattern.group(1)
+                    self._fasta[header] = {"name": header, "beg": int(h_pattern.group(2)), "end": int(h_pattern.group(3)), "seq": ""}
+                elif header:
+                    self._fasta[header]["seq"] += re.sub("[^A-Za-z*]", "", line)
+                else:
+                    raise ValueError('Wrong FASTA format!')
+    def get(self, header=None):
+        if header:
+            if header in self._fasta:
+                return self._fasta[header]
+        else:
+            return self._fasta.values()
+        return None
+# end of class FastaParser
